a175858a45684508267a8b9da3795ce05d9da3ae
Mauro Zurlo <mauro.e.zurlo@gmail.com>
2024-03-25 22:50:09 -0300

    new map base

--- Assets/Scripts/savePoint.cs
+++ Assets/Scripts/savePoint.cs
@@ -3,4 +3,11 @@
 public class savePoint : MonoBehaviour
 {
     public int savePointID;
+
+    void OnDrawGizmos()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, 5);
+    }
 }
