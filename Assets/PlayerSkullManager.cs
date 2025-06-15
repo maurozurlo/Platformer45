623894a75677ffa299d58d651784f193cf3bb1bd
Mauro Zurlo <mauro.e.zurlo@gmail.com>
2025-06-15 11:57:44 -0300

    ignite head

---
+++ Assets/PlayerSkullManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SKULL_STATE
+{
+	idle,
+	on_fire
+}
+public class PlayerSkullManager : MonoBehaviour
+{
+	GameObject FireParticle;
+	SKULL_STATE CurrentState;
+
+
+	private void Start()
+	{
+		FireParticle = transform.Find("FireBall").gameObject; // TODO: maybe make this less hardcoded
+	}
+
+	public void ChangeState(SKULL_STATE state)
+	{
+		if (CurrentState != state)
+		{
+			CurrentState = state;
+			switch (state)
+			{
+				case SKULL_STATE.on_fire:
+					FireParticle.SetActive(true);
+					break;
+				case SKULL_STATE.idle:
+				default:
+					FireParticle.SetActive(false);
+					break;
+			}
+		}
+	}
+}
---
+++ Assets/SkullJumpPad.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullJumpPad : MonoBehaviour
+{
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("PlayerSkull"))
+		{
+			var rb = other.GetComponent<Rigidbody>();
+
+			rb.AddForce(Vector3.up * 35, ForceMode.Impulse);
+
+			other.GetComponent<PlayerSkullManager>().ChangeState(SKULL_STATE.on_fire);
+		}
+	}
+}
